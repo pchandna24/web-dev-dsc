9cc587da84ef502baed964ed34862607dca95bcb
Parth Chandna <pchandna24@gmail.com>
2019-09-25 01:33:36 +0530

    leftover

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var btn = document.getElementById("btn")
 var msn = document.getElementById('mobile-side-nav')
 var sn = document.getElementById('side-nav')
+var root = document.getElementById('root')
 
 
 
@@ -11,10 +12,17 @@ btn.addEventListener("click", (e)=>{
     if(style_msn == "block"){
         msn.style.display = "none"
         sn.style.display = "block"
+        root.style.width = `${screen.width - 300}px`
+        console.log(`${screen.width - 300}px`)
+        root.style.left = "300px"
 
     }
     else{
         msn.style.display = "block"
         sn.style.display = "none"
+        root.style.width = `${screen.width - 100}px`
+        console.log(`${screen.width - 100}px`)
+        root.style.left = "100px"
     }
 })
+
